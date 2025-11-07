5bcef1aac3fe4ffdffa2d5e132068663456e8f45
ThengdeNitin <nitinthengde14368@gmail.com>
2025-11-07 13:41:23 +0530

    Reapply "bug fixed"
    This reverts commit b694e7bb13a6fda6d8b7a621ce7f7e71ae51eacf.

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,32 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const path = require("path");
+const fs = require("fs");
 
 const productsRouter = require("./routes/products");
 const cartRouter = require("./routes/cart");
 const checkoutRouter = require("./routes/checkout");
+const ordersRouter = require("./routes/order.js");
 
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
+const uploadsDir = path.join(__dirname, "uploads");
+
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+  console.log("'uploads' folder created");
+}
+
+app.use("/uploads", express.static(uploadsDir));
+
 app.use("/api/products", productsRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/checkout", checkoutRouter);
+app.use("/api/order", ordersRouter);
 
 const PORT = process.env.PORT || 5000;
 connectDB(process.env.MONGO_URI).then(() => {
