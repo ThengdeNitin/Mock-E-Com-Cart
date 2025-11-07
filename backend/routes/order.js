be131baeb0c6cf74b96b55ceeec3ecb2768d4740
ThengdeNitin <nitinthengde14368@gmail.com>
2025-11-07 13:52:07 +0530

    history error fixed

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Order = require("../models/Order");
 
-router.get("/order", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const orders = await Order.find()
-      .populate("items.productId", "name price image") 
+      .populate("items.productId", "name price image")
       .sort({ createdAt: -1 });
 
     res.json(orders);
@@ -14,6 +14,5 @@ router.get("/order", async (req, res) => {
     res.status(500).json({ error: "Failed to fetch orders" });
   }
 });
-
 
 module.exports = router;
